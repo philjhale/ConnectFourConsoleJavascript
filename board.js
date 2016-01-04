cf5bdc41e5f29b3277f4596e8c15798b6e9e8543
Phil Hale <philjhale@gmail.com>
2016-01-04 21:53:24 +0000

    Various improvements

--- board.js
+++ board.js
@@ -3,99 +3,112 @@
 function Board(numberOfColumns, numberOfRows) {
   this.numberOfColumns = numberOfColumns || 7;
   this.numberOfRows = numberOfRows || 6;
-  var _grid = null;
+  var grid = null;
+  var self = this;
+
+  function createGrid(width, height) {
+    var x, y;
+    var column = [];
+    var rows = [];
+    for (x = 0; x < width; x += 1) {
+      column = [];
+      for (y = 0; y < height; y += 1) {
+        column.push("-");
+      }
+      rows.push(column);
+    }
+
+    return rows;
+  }
+
+  grid = createGrid(this.numberOfColumns, this.numberOfRows);
+
+  this.setDiscAt = function (x, y, disc) {
+    grid[x][y] = disc;
+  };
+
+  this.getGrid = function () {
+    return grid;
+  };
+
+  this.isNumeric = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+
+  this.isColumnFull = function (columnIndex) {
+    var y;
+    for (y = 0; y < this.numberOfRows; y += 1) {
+      if (self.getDiscAt(columnIndex, y) === '-') { // TODO Not a great check
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  this.isValidDrop = function (columnIndexStr) {
+    var columnIndex;
+
+    if (!self.isNumeric(columnIndexStr)) {
+      return false;
+    }
+
+    columnIndex = Number(columnIndexStr);
+    if (columnIndex < 0 || columnIndex >= self.numberOfColumns) {
+      return false;
+    }
+
+    if (self.isColumnFull(columnIndex)) {
+      return false;
+    }
+
+    return true;
+  };
+
+  this.getNextEmptyRowIndex = function (columnIndex) {
+    var y;
+    for (y = 0; y < self.numberOfRows; y += 1) {
+      if (self.getDiscAt(columnIndex, y) === '-') { // TODO Not a great check
+        return y;
+      }
+    }
+
+    // TODO Change?
+    throw "Column full";
+  };
 }
 
-Board.prototype._createGrid = function _createGrid(width, height) {
-  var x, y;
-  var column = [];
-  var rows = [];
-  for (x = 0; x < width; x += 1) {
-    column = [];
-    for (y = 0; y < height; y += 1) {
-      column.push("-");
-    }
-    rows.push(column);
+Board.prototype.drop = function drop(disc, columnNumber) {
+  var columnIndex = Number(columnNumber) - 1;
+  
+  // TODO Check valid move
+  if (!this.isValidDrop(columnIndex)) {
+    throw "Errors!"; // TODO
   }
 
-  return rows;
+  // TODO Need to convert to number or at least check
+  
+
+  //  TODO Place in correct place
+  this.setDiscAt(columnIndex, this.getNextEmptyRowIndex(columnIndex), disc);
 };
 
-// TODO Check my usage of this
-// TODO Don't like this
-Board.prototype._getGrid = function _getGrid() {
-  if (!this._grid) {
-    this._grid = this._createGrid(this.numberOfColumns, this.numberOfRows);
-  }
-
-  return this._grid;
+Board.prototype.getDiscAt = function getDiscAt(x, y) {
+  //console.log(x + ' ' + y);
+  return this.getGrid()[x][y];
 };
 
 Board.prototype.display = function display() {
   var x, y;
   var line = '';
-  for (y = this._getGrid()[0].length - 1; y >= 0; y -= 1) {
-    for (x = 0; x < this._getGrid().length; x += 1) {
-      line += this._getGrid()[x][y] + '\t';
+  for (y = this.numberOfRows - 1; y >= 0; y -= 1) {
+    for (x = 0; x < this.numberOfColumns; x += 1) {
+      line += this.getDiscAt(x, y) + '\t';
     }
     console.log(line);
     line = '';
   }
   console.log("----------------------------------");
-};
-
-Board.prototype._isNumeric = function _isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-Board.prototype._isColumnFull = function _isColumnFull(columnIndex) {
-  for(var y = 0; y < this.numberOfRows; y += 1) {
-    if (this._getGrid()[columnIndex][y] === '-') { // TODO Not a great check
-      return false;
-    }
-  }
-  
-  return true;
-}
-
-Board.prototype._isValidDrop = function _isValidDrop(columnIndexStr) {
-  var columnIndex;
-
-  if (!this._isNumeric(columnIndexStr)) {
-    return false;
-  }
-
-  columnIndex = Number(columnIndexStr);
-  if (columnIndex < 0 || columnIndex > this._getGrid().length) {
-    return false;
-  }
-
-  if (this._isColumnFull(columnIndex)) {
-    return false;
-  }
-
-  return true;
-};
-
-Board.prototype._getNextEmptyRowIndex = function _getNextEmptyRowIndex(columnIndex) {  
-  for(var y = 0; y < this.numberOfRows; y += 1) {
-    if (this._getGrid()[columnIndex][y] === '-') { // TODO Not a great check
-      return y;
-    }
-  }
-  
-  // TODO Change?
-  throw "Column full";
-};
-
-Board.prototype.drop = function drop(disc, columnNumber) {
-  // TODO Check valid move
-  var columnIndex = columnNumber - 1;
-
-  //  TODO Place in correct place
-  this._getGrid()[columnIndex][this._getNextEmptyRowIndex(columnIndex)] = disc;
-
-  return this._getGrid();
 };
 
 module.exports = Board;
--- test/board-tests.js
+++ test/board-tests.js
@@ -6,22 +6,22 @@ var Board = require("../board");
 
 describe("Board", function() {
   var board = new Board(7, 6);
-  
+
   beforeEach(function() {
     board = new Board(7, 6);
   });
-  
+
   describe("isValidDrop", function() {
     it("cannot drop into non numeric column", function() {
-      expect(board._isValidDrop("bumclouds")).to.equal(false);
+      expect(board.drop.bind(board, "R", "bumclouds")).to.throw("Errors!");
     });
 
     it("cannot drop into column index -1", function() {
-      expect(board._isValidDrop("-1")).to.equal(false);
+      expect(board.drop.bind(board, "R", "-1")).to.throw("Errors!");
     });
 
     it("cannot drop into column index greater than size of board", function() {
-      expect(board._isValidDrop("8")).to.equal(false);
+      expect(board.drop.bind(board, "R", "8")).to.throw("Errors!");
     });
 
     it("cannot drop into full column", function() {
@@ -29,29 +29,24 @@ describe("Board", function() {
           board.drop("R", "1")
       }
 
-      expect(board._isValidDrop("0")).to.equal(false);
+      expect(board.drop.bind(board, "R", "0")).to.throw("Errors!");
     });
 
     it("can drop into first column", function() {
-      expect(board._isValidDrop("0")).to.equal(true);
+      board.drop("red", "1");
+      expect(board.getDiscAt(0, 0)).to.equal("red");
     });
 
     it("can drop into last column", function() {
-      expect(board._isValidDrop("6")).to.equal(true);
-    });
-  });
-
-  describe("drop", function() {
-    it("can drop one", function() {
-      var grid = board.drop("red", "1");
-      expect(grid[0][0]).to.equal("red");
+      board.drop("red", "7");
+      expect(board.getDiscAt(6, 0)).to.equal("red");
     });
 
     it("can drop two", function() {
       board.drop("R", "1");
-      var grid = board.drop("R", "1");
-      expect(grid[0][0]).to.equal("R");
-      expect(grid[0][1]).to.equal("R");
+      board.drop("R", "1");
+      expect(board.getDiscAt(0, 0)).to.equal("R");
+      expect(board.getDiscAt(0, 1)).to.equal("R");
     });
   });
 });
